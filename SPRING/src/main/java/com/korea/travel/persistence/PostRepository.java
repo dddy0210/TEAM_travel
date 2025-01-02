5e5b5bc931bdf6371547b0f83496d46a66370ea0
dayy210 <dddy0210@naver.com>
2025-01-02 11:13:58 +0900

    Delete SPRING/src directory

--- SPRING/src/main/java/com/korea/travel/persistence/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.korea.travel.persistence;
-
-import com.korea.travel.model.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
-}
